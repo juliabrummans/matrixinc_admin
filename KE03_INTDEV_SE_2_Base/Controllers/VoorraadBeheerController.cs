00af22b43856e37d7c16323f75ed46258d124517
jill <2303677schleipfenbauer@zuyd.nl>
2026-05-28 13:49:40 +0200

    html bewerkt

--- KE03_INTDEV_SE_2_Base/Controllers/VoorraadBeheerController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/VoorraadBeheerController.cs
@@ -1,6 +1,24 @@
-﻿namespace KE03_INTDEV_SE_2_Base.Controllers
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
 {
-    public class VoorraadBeheerController
+    public class VoorraadBeheerController : Controllers
     {
+        private readonly MatrixIncDbContext _context;
+
+        public VoorraadBeheerController(MatrixIncDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public IActionResult = Index()
+        {
+            var producten = _context.Products.ToList();
+            return View(producten);
+
+        }
     }
 }
