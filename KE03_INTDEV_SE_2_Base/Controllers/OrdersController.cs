ace1302f6762b7a380f8c683d412991c74728ebf
Julia <julia.brummans@gmail.com>
2026-05-28 13:18:59 +0200

    orders pagina toegevoegd

---
+++ KE03_INTDEV_SE_2_Base/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using KE03_INTDEV_SE_2_Base.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var orders = new List<Order>
+            {
+                new Order { Id = 1, ProductNaam = "Laptop", Aantal = 1, Datum = DateTime.Now },
+                new Order { Id = 2, ProductNaam = "Muis", Aantal = 2, Datum = DateTime.Now }
+            };
+
+            return View(orders);
+        }
+    }
+}
---
+++ KE03_INTDEV_SE_2_Base/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace KE03_INTDEV_SE_2_Base.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ProductNaam { get; set; }
+        public int Aantal { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
