f548b3bc38a479ac03a4bf3259692e4f2be516c1
Dax Lacroix <daxlacroix@gmail.com>
2026-06-04 12:52:20 +0200

    dashboard aangepast

--- KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
 {
     public class HomeController : Controller
     {
-        // centrale dashboard met mock data.
+        // GET: Home/Index
         public IActionResult Index()
         {
             return View();
