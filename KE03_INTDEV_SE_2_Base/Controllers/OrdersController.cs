c4fa30ad6d23251966623fee6b5fa0c728d5f72b
Julia <julia.brummans@gmail.com>
2026-06-04 09:35:11 +0200

    order opties toegevoegd

--- KE03_INTDEV_SE_2_Base/Controllers/OrdersController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/OrdersController.cs
@@ -1,19 +1,146 @@
-﻿using KE03_INTDEV_SE_2_Base.Models;
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace KE03_INTDEV_SE_2_Base.Controllers
 {
     public class OrdersController : Controller
     {
-        public IActionResult Index()
+        private readonly MatrixIncDbContext _context;
+
+        public OrdersController(MatrixIncDbContext context)
         {
-            var orders = new List<Order>
-            {
-                new Order { Id = 1, ProductNaam = "Laptop", Aantal = 1, Datum = DateTime.Now },
-                new Order { Id = 2, ProductNaam = "Muis", Aantal = 2, Datum = DateTime.Now }
-            };
+            _context = context;
+        }
+
+        // INDEX
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Products)
+                .ToListAsync();
 
             return View(orders);
         }
+
+        // CREATE GET
+        public IActionResult Create()
+        {
+            ViewBag.Customers = new SelectList(_context.Customers, "Id", "Name");
+            ViewBag.Products = new SelectList(_context.Products, "Id", "Name");
+
+            return View();
+        }
+
+        // CREATE POST (SIMPEL)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int customerId, int productId, DateTime orderDate)
+        {
+            var customer = await _context.Customers.FindAsync(customerId);
+            var product = await _context.Products.FindAsync(productId);
+
+            if (customer == null || product == null)
+            {
+                return NotFound();
+            }
+
+            var order = new Order
+            {
+                CustomerId = customerId,
+                OrderDate = orderDate
+            };
+
+            order.Products.Add(product);
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // DETAILS
+        public async Task<IActionResult> Details(int id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Products)
+                .FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order == null) return NotFound();
+
+            return View(order);
+        }
+
+        // DELETE
+        public async Task<IActionResult> Delete(int id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.Customer)
+                .FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order == null) return NotFound();
+
+            return View(order);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.Products)
+                .FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order != null)
+            {
+                order.Products.Clear();
+                _context.Orders.Remove(order);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.Products)
+                .FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order == null) return NotFound();
+
+            ViewBag.Customers = new SelectList(_context.Customers, "Id", "Name");
+            ViewBag.Products = new SelectList(_context.Products, "Id", "Name");
+
+            return View(order);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Order order)
+        {
+            if (id != order.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Customers = new SelectList(_context.Customers, "Id", "Name", order.CustomerId);
+            ViewBag.Products = new SelectList(_context.Products, "Id", "Name");
+
+            return View(order);
+        }
+
+
+
     }
 }
