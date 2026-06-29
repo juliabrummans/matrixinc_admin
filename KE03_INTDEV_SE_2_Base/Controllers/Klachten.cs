1d7426af110b9be29694b0f0e3a557937dca577c
Maiwand Arman <maiwandenhillah@gmail.com>
2026-06-29 11:37:54 +0200

    klikbaar

---
+++ KE03_INTDEV_SE_2_Base/Controllers/Klachten.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class KlachtenController : Controller
+    {
+        // GET: Home/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
