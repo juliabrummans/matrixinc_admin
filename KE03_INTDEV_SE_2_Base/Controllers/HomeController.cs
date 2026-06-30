930e2d7896ab502f3d79fa4647b33f63fb73884a
Dax Lacroix <daxlacroix@gmail.com>
2026-06-30 07:50:48 +0200

    test

--- KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
@@ -1,13 +1,12 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KE03_INTDEV_SE_2_Base.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home/Index
         public IActionResult Index()
         {
+            // Laadt het originele dashboard met de vierkanten
             return View();
         }
 
---
+++ KE03_INTDEV_SE_2_Base/Models/OrderPickViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace KE03_INTDEV_SE_2_Base.Models
+{
+    public class OrderPickViewModel
+    {
+        public int OrderId { get; set; }
+        public string? DropLocation { get; set; }
+        public string? Status { get; set; }
+        public string? StatusColor { get; set; } // Ontbrekende eigenschap toegevoegd
+        public List<PickItemViewModel> Items { get; set; } = new List<PickItemViewModel>(); // Standaard lege lijst
+    }
+
+    public class PickItemViewModel
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string? ProductLocation { get; set; }
+        public bool IsPicked { get; set; }
+    }
+}
